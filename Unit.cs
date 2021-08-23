c1ade9e6b2c1bb59cf7296adfaad46a10f841f3e
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2021-08-23 15:49:00 +0800

    Update Unit.cs

--- Unit.cs
+++ Unit.cs
@@ -7,4 +7,17 @@ public class Unit : MonoBehaviour {
     public int Damage;
     public int MaxHP;
     public int CurrentHP;
+
+    public void TakeDamage(int Amount) {
+        CurrentHP -= Amount;
+    }
+
+    public void Heal(int Amount) {
+        CurrentHP += Amount;
+        if(CurrentHP > MaxHP) CurrentHP = MaxHP;
+    }
+
+    public bool Alive() {
+        if(CurrentHP > 0) return true; else return false;
+    }
 }
